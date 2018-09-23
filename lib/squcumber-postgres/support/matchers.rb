b3e1a3828ed9ca5201d758a5a90b1fcc2083ae5e
Stefanie Grunwald <stefanie.grunwald@adobe.com>
2018-09-23 13:59:36 +0200

    Add sophisticated date matchers
    This commit adds date matchers with the following modifiers:
     - beginning/end of day/month/year
     - X days/months/years ago/from now
     - (as day/month/year/custom)
    
    Which can be combined freely to e.g.:
     - beginning of month 7 years from now (as day)
     - end of year 10 months ago (as custom '%Y/%m/%d')
    
    The custom formatter uses Ruby formatting:
    https://apidock.com/ruby/DateTime/strftime

--- lib/squcumber-postgres/support/matchers.rb
+++ lib/squcumber-postgres/support/matchers.rb
@@ -23,23 +23,127 @@ module MatcherHelpers
   def convert_mock_values(mock_data)
     mock_data.map do |entry|
       entry.each do |key, value|
-        entry[key] = case value
-          when /today/
-            Date.today.to_s
-          when /yesterday/
-            Date.today.prev_day.to_s
-          when /\s*\d+\s+month(s)?\s+ago\s*/
-            number_of_months = value.match(/\d+/)[0].to_i
-            timetravel(Date.today, number_of_months, :prev_month).to_s
-          when /\s*\d+\s+day(s)?\s+ago\s*/
-            number_of_days = value.match(/\d+/)[0].to_i
-            timetravel(Date.today, number_of_days, :prev_day).to_s
-          else
-            value
-        end
+        # Examples of valid values; all examples assume that today is 30th July 2017
+        #
+        #    1 month ago
+        #    => '2017-06-30'
+        #
+        #    40 days ago (as month)
+        #    => '6'
+        #
+        #    2 years ago (as year)
+        #    => '2015'
+        #
+        #    beginning of last month
+        #    => '2017-06-01'
+        #
+        #    end of last year
+        #    => '2016-12-31'
+        #
+        #    today (as custom '%Y-%m')
+        #    => '2017-07'
+        #
+        entry[key] = convert_mock_value(value)
       end
+    end
+  end
+
+  def convert_mock_value(value)
+    value_parser_regexp = /\s*((?<modifier>(beginning|end))\s+of\s+(?<modifier_base>day|month|year))?\s*(?<placeholder>[^\(\)]+)\s*(\(as (?<format>day|month|year|(custom '[^']+'))\))?\s*/
+
+    parsed_value = value.match(value_parser_regexp)
+    placeholder = parsed_value[:placeholder]
+    format = parsed_value[:format]
+    modifier = parsed_value[:modifier]
+    modifier_base = parsed_value[:modifier_base]
+
+    new_value = case placeholder
+      when /today/
+        Date.today
+      when /yesterday/
+        timetravel(Date.today, 1, :prev_day)
+      when /tomorrow/
+        timetravel(Date.today, 1, :next_day)
+      when /last month/
+        timetravel(Date.today, 1, :prev_month)
+      when /next month/
+        timetravel(Date.today, 1, :next_month)
+      when /last year/
+        timetravel(Date.today, 1, :prev_year)
+      when /next year/
+        timetravel(Date.today, 1, :next_year)
+      when /\s*\d+\s+month(s)?\s+ago\s*?/
+        number_of_months = value.match(/\d+/)[0].to_i
+        timetravel(Date.today, number_of_months, :prev_month)
+      when /\s*\d+\s+day(s)?\s+ago\s*/
+        number_of_days = value.match(/\d+/)[0].to_i
+        timetravel(Date.today, number_of_days, :prev_day)
+      when /\s*\d+\s+year(s)?\s+ago\s*/
+        number_of_years = value.match(/\d+/)[0].to_i
+        timetravel(Date.today, number_of_years, :prev_year)
+      when /\s*\d+\s+month(s)?\s+from now\s*?/
+        number_of_months = value.match(/\d+/)[0].to_i
+        timetravel(Date.today, number_of_months, :next_month)
+      when /\s*\d+\s+day(s)?\s+from now\s*/
+        number_of_days = value.match(/\d+/)[0].to_i
+        timetravel(Date.today, number_of_days, :next_day)
+      when /\s*\d+\s+year(s)?\s+from now\s*/
+        number_of_years = value.match(/\d+/)[0].to_i
+        timetravel(Date.today, number_of_years, :next_year)
+      else
+        placeholder
+    end
+
+    if new_value.is_a?(Date)
+      modified_new_value = case modifier
+        when nil
+          new_value
+        when 'beginning'
+          case modifier_base
+            when 'day'
+              new_value
+            when 'month'
+              Date.new(new_value.year, new_value.month, 1)
+            when 'year'
+              Date.new(new_value.year, 1, 1)
+            else
+              raise "Invalid date modifier provided: #{modifier} #{modifier_base}"
+          end
+        when 'end'
+          case modifier_base
+            when 'day'
+              new_value
+            when 'month'
+              Date.new(new_value.next_month.year, new_value.next_month.month, 1).prev_day
+            when 'year'
+              Date.new(new_value.next_year.year, 1, 1).prev_day
+            else
+              raise "Invalid date modifier provided: #{modifier} #{modifier_base}"
+          end
+        else
+          raise "Invalid date modifier provided: #{modifier} #{modifier_base}"
+      end
+
+      formatted_new_value = case format
+        when nil
+          puts "NO FORMAT"
+          modified_new_value.to_s
+        when 'day', 'month', 'year'
+          modified_new_value.send(format.to_sym)
+        when /custom '[^']+'/
+          parsed_format = format.match(/custom '(?<format_string>[^']+)'/)
+          modified_new_value.strftime(parsed_format[:format_string])
+        else
+          raise "Invalid date format provided: #{format}"
+      end
+
+      formatted_new_value
+    else
+      new_value
     end
   end
 end
 
-World(MatcherHelpers)
+if defined?(World)
+  World(MatcherHelpers)
+end
